f0d0a23ea50495ce45e37630c3e09f1dcd430d77
Stephen Ezea <stephennnabuike@gmail.com>
2024-07-22 08:11:52 +0100

    Update main.py file

--- main.py
+++ main.py
@@ -16,6 +16,11 @@ def get_db():
     finally:
         db.close()
 
+@app.get("/items/", response_model=schemas.Item)
+def read_items(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+    items = db.query(models.Item).offset(skip).limit(limit).all()
+    return items
+
 @app.post("/items/", response_model=schemas.Item)
 def create_item(item: schemas.ItemCreate, db: Session = Depends(get_db)):
     db_item = models.Item(**item.dict())
